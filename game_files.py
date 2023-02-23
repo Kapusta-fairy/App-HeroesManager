5ded1d88480c95887f5dd3f638b90e1d2c5f97f0
Kapusta-fairy <dknenorm63@gmail.com>
2023-02-23 21:33:02 +0300

    finished game_files.py

--- game_files.py
+++ game_files.py
@@ -10,42 +10,34 @@ class GameFiles:
         self.directory = None
         self.conf_path = Config().conf_path
 
-    def get_list(self, enabled=True) -> list:
-        """Gives the enabled items by default.
-        :arg enabled: return enabled items if true or disabled if false
-        :return: list of items"""
-        if enabled:
-            path = self.game_path
-        else:
-            path = self.conf_path
+    def get_enabled_list(self) -> list:
+        return os.listdir(f'{self.game_path}\\{self.directory}')
 
-        return os.listdir(f'{path}{self.directory}')
+    def get_disabled_list(self) -> list:
+        return os.listdir(f'{self.conf_path}\\{self.directory}')
 
-    def enable(self):
-        pass
+    def enable(self, item_name):
+        return os.replace(
+            f'{self.conf_path}\\{self.directory}\\{item_name}',
+            f'{self.game_path}\\{self.directory}\\{item_name}'
+        )
+
+    def disable(self, item_name):
+        return os.replace(
+            f'{self.game_path}\\{self.directory}\\{item_name}',
+            f'{self.conf_path}\\{self.directory}\\{item_name}'
+        )
 
 
 class Mods(GameFiles):
 
     def __init__(self):
         super().__init__()
-        self.directory = r'\UserMODs'
-
-    def enable(self):
-        pass
-
-    def remove(self, mod_name):
-        pass
+        self.directory = r'UserMODs'
 
 
 class Maps(GameFiles):
 
     def __init__(self):
         super().__init__()
-        self.directory = r'\Maps'
-
-    def add(self, map_name):
-        pass
-
-    def remove(self, map_name):
-        pass
+        self.directory = r'Maps'
